569b44aeb90d58c3cdccdb2702dbbb00ff975cd1
CodingWithDude <codingwithdude@gmail.com>
2022-07-04 03:45:40 -0700

    Had to rewrite all main.js and update index.html
    Previous main.js did not account for adding multiple numbers
    Rewrote calculator as a class with seperate functions for
    operands, updating display, and handling functions.
    
    reworked index.html to use data-types

--- main.js
+++ main.js
@@ -1,211 +1,130 @@
-// takes onclick and simulates keydown
-function simulateKeydown(value) {
-  window.dispatchEvent(
-    new KeyboardEvent("keydown", {
-      key: value,
-    })
-  );
-  window.dispatchEvent(
-    new KeyboardEvent("keyup", {
-      key: value,
-    })
-  );
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
 }
 
-// main program
-window.addEventListener("load", function () {
-  const displayCurrent = document.querySelector(".displayCurrent");
-  const displayLast = document.querySelector(".displayLast");
-  let input = "";
-  let num1 = null;
-  let num2 = null;
-  let operator = "";
-  let newState = true;
-  let decimalState = false;
-  let zero = document.getElementById("0");
-  let one = document.getElementById("1");
-  let two = document.getElementById("2");
-  let three = document.getElementById("3");
-  let four = document.getElementById("4");
-  let five = document.getElementById("5");
-  let six = document.getElementById("6");
-  let seven = document.getElementById("7");
-  let eight = document.getElementById("8");
-  let nine = document.getElementById("9");
-  let deleteBtn = document.getElementById("deleteBtn");
-  let backspaceBtn = document.getElementById("backspaceBtn");
-  let divide = document.getElementById("/");
-  let multiply = document.getElementById("*");
-  let add = document.getElementById("+");
-  let subtract = document.getElementById("-");
-  let decimal = document.getElementById(".");
-  let enter = document.getElementById("Enter");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const backspaceButton = document.querySelector("[data-backspace]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
 
-  window.addEventListener("keydown", (e) => {
-    // check if starting new function
-    if (!newState) {
-      displayCurrent.innerText = "";
-      displayLast.innerText = "";
-      newState = true;
-    }
-    // for numeric input
-    if (e.key === "0" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      zero.classList.add("active");
-    }
-    if (e.key === "1" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      one.classList.add("active");
-    }
-    if (e.key === "2" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      two.classList.add("active");
-    }
-    if (e.key === "3" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      three.classList.add("active");
-    }
-    if (e.key === "4" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      four.classList.add("active");
-    }
-    if (e.key === "5" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      five.classList.add("active");
-    }
-    if (e.key === "6" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      six.classList.add("active");
-    }
-    if (e.key === "7" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      seven.classList.add("active");
-    }
-    if (e.key === "8" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      eight.classList.add("active");
-    }
-    if (e.key === "9" && newState) {
-      input += e.key;
-      displayCurrent.innerText = input;
-      nine.classList.add("active");
-    }
-    if (e.key === "." && newState && !decimalState) {
-      decimalState = true;
-      input += e.key;
-      displayCurrent.innerText = input;
-      decimal.classList.add("active");
-    }
-    // for clear button
-    if (e.key === "Delete") {
-      input = "";
-      displayCurrent.innerText = input;
-      displayLast.innerText = input;
-      reset();
-      deleteBtn.classList.add("active");
-    }
-    // for backspace button
-    if (e.key === "Backspace") {
-      if (input.slice(-1) === ".") {
-        decimalState = false;
-      }
-      input = input.slice(0, -1);
-      displayCurrent.innerText = input;
-      backspaceBtn.classList.add("active");
-    }
-    // for addition
-    if (e.key === "+" && operator === "") {
-      decimalState = false;
-      num1 = Number(input);
-      displayCurrent.innerText = "";
-      displayLast.innerText = `${input} +`;
-      add.classList.add("active");
-      input = "";
-      operator = "+";
-    }
-    // for subtraction
-    if (e.key === "-" && operator === "") {
-      decimalState = false;
-      num1 = Number(input);
-      displayCurrent.innerText = "";
-      displayLast.innerText = `${input} -`;
-      subtract.classList.add("active");
-      input = "";
-      operator = "-";
-    }
-    // for division
-    if (e.key === "/" && operator === "") {
-      decimalState = false;
-      num1 = Number(input);
-      displayCurrent.innerText = "";
-      displayLast.innerText = `${input} /`;
-      divide.classList.add("active");
-      input = "";
-      operator = "/";
-    }
-    // for multiplication
-    if (e.key === "*" && operator === "") {
-      decimalState = false;
-      num1 = Number(input);
-      displayCurrent.innerText = "";
-      displayLast.innerText = `${input} *`;
-      multiply.classList.add("active");
-      input = "";
-      operator = "*";
-    }
-    // for enter
-    if (e.key === "Enter" && operator === "+") {
-      num2 = Number(displayCurrent.innerText);
-      displayCurrent.innerText = num1 + Number(displayCurrent.innerText);
-      displayLast.innerText = `${num1} + ${num2} =`;
-      enter.classList.add("active");
-      reset();
-    }
-    if (e.key === "Enter" && operator === "-") {
-      num2 = Number(displayCurrent.innerText);
-      displayCurrent.innerText = num1 - Number(displayCurrent.innerText);
-      displayLast.innerText = `${num1} - ${num2} =`;
-      enter.classList.add("active");
-      reset();
-    }
-    if (e.key === "Enter" && operator === "/") {
-      num2 = Number(displayCurrent.innerText);
-      displayCurrent.innerText = num1 / Number(displayCurrent.innerText);
-      displayLast.innerText = `${num1} / ${num2} =`;
-      enter.classList.add("active");
-      reset();
-    }
-    if (e.key === "Enter" && operator === "*") {
-      num2 = Number(displayCurrent.innerText);
-      displayCurrent.innerText = num1 * Number(displayCurrent.innerText);
-      displayLast.innerText = `${num1} * ${num2} =`;
-      enter.classList.add("active");
-      reset();
-    }
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
   });
+});
 
-  window.addEventListener("keyup", (e) => {
-    elements = document.getElementsByClassName("active");
-    for (element of elements) {
-      element.classList.remove("active");
-    }
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
   });
+});
 
-  function reset() {
-    input = "";
-    num1 = null;
-    num2 = null;
-    operator = "";
-    newState = false;
-    decimalState = false;
-  }
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+backspaceButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
 });
